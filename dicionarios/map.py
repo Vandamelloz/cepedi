e7d26e03346042c451a9ebdf7e9bb6f3eb6c81a6
Vandamelloz <vandabarsa2004@gmail.com>
2025-09-26 16:58:26 -0300

    aula atualização5

---
+++ dicionarios/map.py
@@ -0,0 +1,12 @@
+dicionario= {"nome": "Maria", "idade": 21, "peso": 72.8}
+print(dicionario["nome"])
+
+for n, m in dicionario.items():
+    print(f"{n} - {m}")
+
+
+
+'''if "idade" in dicionario:'''
+
+
+  
